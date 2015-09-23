48736f34fba416e1945f04883d220069e2bd304c
twinkle <twinklemevada@jacksolutions.biz>
2015-09-23 17:14:25 +0530

    config js file

--- www/cellidinfoplugin.js
+++ www/cellidinfoplugin.js
@@ -1,36 +1,29 @@
 /**
  * @constructor
  */
-var SimLocationLoader = function (require, exports, module) {
-    var exec = require("cordova/exec");
-    
-	var cellidinfoplugin = function(){};
+var cellidinfoplugin = function(){};
 
-	cellidinfoplugin.prototype.getCellInfo = function(successCallback, failure){
-		cordova.exec(successCallback, failure, "CellIdInfoPlugin", "getCellInfo", []);
-	};
-
-	cellidinfoplugin.prototype.getGeoCords = function(successCallback, failure){
-	  cordova.exec(successGeoCords, failure, "CellIdInfoPlugin", "getGeoCords", []);
-	};
-
-	// This callback function will be executed depending on device location i.e indoor or outdoor and may take 30 seconds to 20 minutes.
-	function successGeoCords(result){
-		alert(JSON.stringify(result));
-	}
-
-	// Plug into Cordova
-	cordova.addConstructor(function() {
-
-	    if (!window.Cordova) {
-	        window.Cordova = cordova;
-	    };
-
-	    if(!window.plugins) window.plugins = {};
-	    window.plugins.CellIdInfoPlugin = new CellIdInfoPlugin();
-	    return window.plugins.CellIdInfoPlugin;
-	});
+cellidinfoplugin.prototype.getCellInfo = function(successCallback, failure){
+	cordova.exec(successCallback, failure, "CellIdInfoPlugin", "getCellInfo", []);
 };
-SimLocationLoader(require, exports, module);
 
-cordova.define("cordova/plugin/cellidinfoplugin", SimLocationLoader);
+cellidinfoplugin.prototype.getGeoCords = function(successCallback, failure){
+  cordova.exec(successGeoCords, failure, "CellIdInfoPlugin", "getGeoCords", []);
+};
+
+// This callback function will be executed depending on device location i.e indoor or outdoor and may take 30 seconds to 20 minutes.
+function successGeoCords(result){
+	alert(JSON.stringify(result));
+}
+
+// Plug into Cordova
+cordova.addConstructor(function() {
+
+    if (!window.Cordova) {
+        window.Cordova = cordova;
+    };
+
+    if(!window.plugins) window.plugins = {};
+    window.plugins.CellIdInfoPlugin = new CellIdInfoPlugin();
+   	return window.plugins.CellIdInfoPlugin;
+});
